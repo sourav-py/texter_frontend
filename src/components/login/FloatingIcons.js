2dbb40f31a9892bb03573656138c2e4c80024eb4
Sourav Singh <imsrv2k@gmail.com>
2024-01-30 22:04:32 +0530

    UI: tailwind css in verify otp screen

--- src/components/login/FloatingIcons.js
+++ src/components/login/FloatingIcons.js
@@ -9,21 +9,21 @@ function FloatingIcons() {
 
     return (
         <>
-            <FontAwesomeIcon className="text-white text-7xl animate-bounce" icon={faComment}/>
+            <FontAwesomeIcon className="text-white text-7xl" icon={faComment}/>
             <FontAwesomeIcon className="text-white text-4xl" icon={faUser}/>
             <FontAwesomeIcon className="text-white text-9xl" icon={faBook}/>
             <FontAwesomeIcon className="text-white text-4xl" icon={faCommenting}/>
-            <FontAwesomeIcon className="text-white text-8xl animate-bounce" icon={faComments}/>
+            <FontAwesomeIcon className="text-white text-8xl" icon={faComments}/>
             <FontAwesomeIcon className="text-white text-7xl" icon={faQuoteLeft}/>
-            <FontAwesomeIcon className="text-white text-4xl animate-bounce" icon={faNewspaper}/>
+            <FontAwesomeIcon className="text-white text-4xl" icon={faNewspaper}/>
             <FontAwesomeIcon className="text-white text-6xl" icon={faQuoteRight}/>
-            <FontAwesomeIcon className="text-white text-3xl animate-bounce" icon={faPaperPlane}/>
+            <FontAwesomeIcon className="text-white text-3xl" icon={faPaperPlane}/>
             <FontAwesomeIcon className="text-white text-8xl" icon={faPaperclip}/>
             <FontAwesomeIcon className="text-white text-6xl" icon={faUsers}/>
-            <FontAwesomeIcon className="text-white text-7xl animate-bounce" icon={faAddressCard}/>
+            <FontAwesomeIcon className="text-white text-7xl" icon={faAddressCard}/>
             <FontAwesomeIcon className="text-white text-3xl" icon={faBookmark}/>
             <FontAwesomeIcon className="text-white text-8xl" icon={faUser}/>
-            <FontAwesomeIcon className="text-white text-6xl animate-bounce" icon={faComment}/>
+            <FontAwesomeIcon className="text-white text-6xl" icon={faComment}/>
             <FontAwesomeIcon className="text-white text-6xl" icon={faQuoteLeft}/>
         </>
     )
